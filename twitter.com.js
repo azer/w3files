8abbb4569402d153795ad8c2142aeee54fa23a11
E.Azer Koçulu <azer@kodfabrik.com>
2013-05-06 00:09:05 -0700

    show media

--- twitter.com.js
+++ twitter.com.js
@@ -1,3 +1,13 @@
-var $ = document.querySelector.bind(document);
+var $ = document.querySelector.bind(document),
+    home = $('.home-tweet-box'),
+    profile = $('.recent_photos');
 
-$('.wrapper').insertBefore($('.home-tweet-box'), $('#timeline'));
+if(home) $('.wrapper').insertBefore($('.home-tweet-box'), $('#timeline'));
+
+profile && setTimeout(function(){
+  document.querySelector('.wrapper').insertBefore(document.querySelector('.recent_photos'), document.querySelector('#timeline'));
+}, 1000);
+
+Array.prototype.forEach.call(document.querySelectorAll('.promoted-tweet'), function(el){
+  el.parentNode.removeChild(el);
+});
